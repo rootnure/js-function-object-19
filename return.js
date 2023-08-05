1ec61010cdff15744710578d7eb7387afbccabaa
rootnure <nur.diu.2791@gmail.com>
2023-08-05 20:44:29 +0600

    Function return

---
+++ return.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    // console.log(num1, num2);
+    var sum = num1 + num2;
+    return sum;
+    console.log('I need more code');
+    return 15;
+    return 'Hello';
+}
+
+add(50, 30);
+
+var total = add(44, 66);
+// console.log(total);
+
+
+function bringSomucha(money){
+    var singaraPrice = 10;
+    var quantity = money / singaraPrice;
+    return quantity;
+}
+
+var somuchas = bringSomucha(500);
+console.log('Eating Somuchas:', somuchas);
