86eddfbae4cf7cdf021a3191a8b5bb263b21eddf
rootnure <nur.diu.2791@gmail.com>
2023-08-05 20:09:09 +0600

    Function parameter (single parameter, multiple parameter)

--- parameter.js
+++ parameter.js
@@ -1,6 +1,22 @@
-function bringSingara(money){
-    console.log('Mama give me singara');
-    console.log(money);
+function bringSomucha(money){
+    console.log(money, 'Taka received.');
+    console.log('Take the singara.');
 }
 
-bringSingara(500);
+var taka = 500;
+
+// bringSomucha(taka);
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+// add(125, 44);
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(23, 45, 44, 54, 16, 85);
