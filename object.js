38c1bff0d422b1546d8ac90130d4ba29141135ee
rootnure <nur.diu.2791@gmail.com>
2023-08-06 02:19:54 +0600

    JS Object intro (get/set by dot notation)

---
+++ object.js
@@ -0,0 +1,26 @@
+const student = {
+    id: 234,
+    name: 'James',
+    class: 4,
+    marks: 98
+};
+
+const mobile = {
+    brand: 'Redmi',
+    model: 'Note 9s',
+    price: 23999,
+    storage: '128GB',
+    camera: '48MP'
+};
+
+const myComputer = {
+    brand: 'DCL',
+    price: 28000,
+    color: 'Silver',
+    processor: 'i3 10th Gen',
+    ram: '16GB'
+}
+
+console.log(myComputer);
+console.log(mobile);
+console.log(mobile.price);
