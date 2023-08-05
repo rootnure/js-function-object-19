c3596a2eb12305e2c2a90e0e496d3d9ca890ee87
rootnure <nur.diu.2791@gmail.com>
2023-08-05 15:48:21 +0600

    function declaration

---
+++ function.js
@@ -0,0 +1,13 @@
+// function declaration
+function startFan() {
+    console.log('Stand Up');
+    console.log('Walk towards the switch');
+    console.log('Press the switch');
+}
+
+// call the function
+startFan();
+console.log('Watching tutorial video');
+startFan();
+console.log('Learning web development');
+startFan();
