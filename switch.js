a8d10fd7e883afcf84e8f9644ce7e4dcaf39e73b
rootnure <nur.diu.2791@gmail.com>
2023-08-07 02:21:55 +0600

    switch-case in js

---
+++ switch.js
@@ -0,0 +1,46 @@
+const color = 'white';
+if (color === 'white') {
+    console.log('The color is white');
+}
+else if (color === 'gray') {
+    console.log('The color is gray');
+}
+else if (color === 'blue') {
+    console.log('The color is blue');
+}
+else if (color === 'red') {
+    console.log('The color is red');
+}
+else if (color === 'blue') {
+    console.log('The color is blue');
+}
+else if (color === 'yellow') {
+    console.log('The color is yellow');
+}
+else {
+    console.log('The color absent');
+}
+
+// switch
+switch(color) {
+    case 'white':
+        console.log('The color is white');
+        break;
+    case 'gray':
+        console.log('The color is gray');
+        break;
+    case 'blue':
+        console.log('The color is blue');
+        break;
+    case 'red':
+        console.log('The color is red');
+        break;
+    case 'blue':
+        console.log('The color is blue');
+        break;
+    case 'yellow':
+        console.log('The color is yellow');
+        break;
+    default:
+        console.log('The color absent');
+}
